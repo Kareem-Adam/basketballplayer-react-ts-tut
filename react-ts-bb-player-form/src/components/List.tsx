3a497f8416f97d7edd79c065060f886d6f8d65e6
Kareem-Adam <82541086+Kareem-Adam@users.noreply.github.com>
2022-01-13 16:46:11 +0000

    feat: add render list function

--- react-ts-bb-player-form/src/components/List.tsx
+++ react-ts-bb-player-form/src/components/List.tsx
@@ -1,21 +1,39 @@
 import React from "react";
 
-  /* interface creates type definitions for a given variable/object */
-  interface IProps{
+/* interface creates type definitions for a given variable/object */
+interface IProps {
     people: {
-      name: string
-      age: number 
-      url: string 
-      note?: string  //? allows property to be optional 
+        name: string
+        age: number
+        url: string
+        note?: string  //? allows property to be optional 
     }[] //[] defines people as type object 
-  }
+}
 
-/* Child component to render player list */
-const List = (props: IProps) => {
+/* Child component to render player list, defines list as functional react component with IProps interface */
+const List: React.FC<IProps> = ({ people }) => {
+
+    /* return type JSX element array */
+    const renderList = (): JSX.Element[] => {
+        /* duplicate and render array values */
+        return people.map(person => {
+            return (
+                <li className="List">
+                    <div className="List-header">
+                        <img className="List-img" src={person.url} />
+                        <h2>{person.age}</h2>
+                    </div>
+                    <p>{person.age} years old</p>
+                    <p className="List-note">{person.note}</p>
+                </li>
+            )
+        })
+    }
+
     return (
-        <div>
-            I am a list
-        </div>
+        <ul>
+            {renderList()}
+        </ul>
     )
 }
 
