2a2f83b64c55db588d6247ef1f16e87dc4e71abc
Kareem-Adam <82541086+Kareem-Adam@users.noreply.github.com>
2022-01-13 18:13:24 +0000

    feat: add form inputs

--- react-ts-bb-player-form/src/components/addToList.tsx
+++ react-ts-bb-player-form/src/components/addToList.tsx
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { JsxAttributeLike } from "typescript";
 
-const AddToList =():JSX.Element =>{
+interface InputProps {
+
+    name: string,
+    age: string,
+    note: string,
+    img: string
+}
+
+const AddToList = (): JSX.Element => {
+
+    const [formInput, setInput] = useState<InputProps>
+        ({
+            name: "",
+            age: "",
+            note: "",
+            img: ""
+        })
+
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setInput({
+        ...formInput, //destructure array so only target value changes 
+            [e.target.name]: e.target.value //override new value  
+        })
+    }
+
     return (<div>
-        add to list component
+        <input type="text"
+            placeholder="Name"
+            className="AddToList-input"
+            value={formInput.name}
+            onChange={handleChange}
+            name="name"
+        />
+        <input type="text"
+            placeholder="Age"
+            className="AddToList-input"
+            value={formInput.age}
+            onChange={handleChange}
+            name="age"
+        />
+        <input type="text"
+            placeholder="Image URL"
+            className="AddToList-input"
+            value={formInput.img}
+            onChange={handleChange}
+            name="img"
+        />
+        <textarea
+            placeholder="Notes"
+            className="AddToList-input"
+            value={formInput.note}
+            onChange={handleChange}
+
+        />
     </div>)
 }
 
